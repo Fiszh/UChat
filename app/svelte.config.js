a4c99ffe2e9a8d0e65973a8aa2ad877d39e34c3b
Killer Fish <61081555+Fiszh@users.noreply.github.com>
2025-12-29 16:32:24 +0100

    Static fix

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -3,20 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null, // 'index.html' if SPA
-            precompress: true
-        }),
-        alias: {
-            $stores: path.resolve('./src/stores'),
-            $components: path.resolve('./src/components')
-        }
-    }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true
+		}),
+		alias: {
+			$stores: path.resolve('./src/stores'),
+			$components: path.resolve('./src/components')
+		}
+	}
 };
 
 export default config;
