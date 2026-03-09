91dcebc997813f8304157326dfd00d4dbba6d82e
kasqo <saboadd010@gmail.com>
2026-03-09 17:40:25 +0000

    Update +error.svelte

--- app/src/routes/+error.svelte
+++ app/src/routes/+error.svelte
@@ -5,67 +5,78 @@
 </script>
 
 <main>
-    <div>
+    <div class="container">
+        <h1 class="AUUU">404</h1>
         <h1>Oops, page not found</h1>
         <p>
             Looks like the page you're trying to reach doesn't exist or was
             moved. <br />
             Check the URL or try one of the options below.
         </p>
+        
         <div id="buttons">
-            <button on:click={goBack}>Go back a page</button>
-            <a href="/">Go back homepage</a>
+            <a href="javascript:history.back()" class="button">Go Back a Page</a>
+            <a href="/" class="button">Go back homepage</a>
+        </div>
+
+        <div class="gif">
+            <img src="https://cdn.unii.dev/404.avif" alt="404" />
         </div>
     </div>
-    <img src="https://cdn.unii.dev/404.avif" alt="404" />
 </main>
 
 <style lang="scss">
     main {
-        width: 100dvw;
-        height: 100dvh;
-        display: flex;
-        padding: 50px;
-        box-sizing: border-box;
-        justify-content: space-between;
-        align-items: center;
+        margin: 0;
         background: linear-gradient(45deg, #080808, #0d0d10);
+        font-family: sans-serif;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
     }
+
+    .container {
+        text-align: center;
+    }
+
     h1 {
-        font-size: 2.5rem;
+        font-size: 50px;
+        margin: 30px 15px;
+    }
+
+    .AUUU {
+        font-size: 100px;
+        margin: 0;
     }
 
     p {
-        width: 40dvw;
+        margin: 15px 25px;
     }
 
-    #buttons {
-        display: flex;
-        gap: 25px;
+    .button {
+        display: inline-block;
+        padding: 12px 28px;
+        margin: 10px 5px;
+        background: #232324;
+        color: white;
+        font-weight: 600;
+        text-decoration: none;
+        border-radius: 12px;
+        transition: all 0.3s ease;
 
-        & > * {
-            all: unset;
-            transition: all 0.3s;
-            padding: 0.75rem 2rem;
-            background: rgba(255 255 255 / 0.1);
-            border: 1px solid #555;
-            border-radius: 12px;
-            color: #eee;
-            cursor: pointer;
-            text-decoration: none;
-            font-size: 1rem;
+        &:hover {
+            transform: translateY(-3px);
+        }
 
-            &:hover {
-                background-color: rgba(255, 255, 255, 0.068);
-                border-radius: 10px;
-                border: 1px white solid;
-            }
+        &:active {
+            transform: translateY(1px);
         }
     }
 
     img {
-        aspect-ratio: 1 / 1;
-        height: 20dvw;
-        border-radius: 10px;
+        margin-top: 25px;
+        width: 200px;
     }
 </style>
