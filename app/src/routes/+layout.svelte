32cba9142633d01d74fdf4249d05ec5e70f21531
Killer Fish <61081555+Fiszh@users.noreply.github.com>
2026-05-28 23:24:45 +0000

    embed test

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -28,15 +28,37 @@
 </script>
 
 {#if mounted}
-    {#if !hasChannel}
-        <main>
-            {#if page.status !== 404 && page.route.id != "/auth"}
-                <Sidebar />
-            {/if}
-            {@render children()}
-        </main>
+    {#if window.self !== window.top && document.referrer}
+        <p id="embed-warning">
+            <span>EMBED OF:</span>
+            <a href={window.location.host} target="_blank"
+                >{window.location.host}</a
+            >
+        </p>
+    {/if}
+
+    {#if window.self !== window.top && !document.referrer}
+        <div id="embed-block">
+            <h1>EMBED METHOD NOT ALLOWED</h1>
+            <span
+                >If you are the owner of this website, please add a referrer.</span
+            >
+            <span>Check out UChat:</span>
+            <a href="https://{window.location.host}" target="_blank"
+                >{window.location.host}</a
+            >
+        </div>
     {:else}
-        {@render children()}
+        {#if !hasChannel}
+            <main>
+                {#if page.status !== 404 && page.route.id != "/auth"}
+                    <Sidebar />
+                {/if}
+                {@render children()}
+            </main>
+        {:else}
+            {@render children()}
+        {/if}
     {/if}
 {:else}
     <main>Loading...</main>
@@ -49,6 +71,33 @@
         height: 100dvh;
 
         background: linear-gradient(#080808, #000000);
+    }
+
+    #embed-block {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+
+        background-color: rgb(111, 20, 20);
+        font-weight: bolder;
+
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        justify-content: center;
+    }
+
+    #embed-warning {
+        position: absolute;
+        z-index: 10000000000000;
+        right: 8px;
+        bottom: 8px;
+        font-weight: bolder;
+        background-color: #141414e1;
+        padding: 7px 10px;
+        white-space: nowrap;
+        border-radius: 16px;
+        font-size: 1rem;
     }
 
     @media (max-width: 768px) {
