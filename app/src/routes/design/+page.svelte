98657ab7486db6b48173d238b63b347ca2738fbb
fiszh <61081555+Fiszh@users.noreply.github.com>
2026-06-19 23:53:40 +0200

    Better design page look

--- app/src/routes/design/+page.svelte
+++ app/src/routes/design/+page.svelte
@@ -20,7 +20,6 @@
 <main>
     <h1>UChat Design Page</h1>
 
-    <p>Buttons</p>
     {#snippet icon()}
         <House size="1rem" />
     {/snippet}
@@ -30,39 +29,55 @@
     {#snippet iconSearch()}
         <Search size="1rem" />
     {/snippet}
-    <Button {icon}>Test Button</Button>
-    <Button {iconRight}>Test Button</Button>
-    <Button>Test Button</Button>
 
-    <p>Toggles</p>
-    <Toggle checked={true} />
-    <Toggle />
-    <Toggle disabled />
+    <section>
+        <p>Buttons</p>
+        <Button {icon}>Test Button</Button>
+        <Button {iconRight}>Test Button</Button>
+        <Button>Test Button</Button>
+    </section>
 
-    <p>Checkboxes</p>
-    <Checkbox checked={true}>Checked</Checkbox>
-    <Checkbox>Unchecked</Checkbox>
+    <section>
+        <p>Toggles</p>
+        <Toggle checked={true} />
+        <Toggle />
+        <Toggle disabled />
+    </section>
 
-    <p>Inputs</p>
-    <Input icon={iconSearch} {iconRight} placeholder="Search..." />
-    <Input disabled />
-    <Input placeholder="Invalid Input" invalid />
+    <section>
+        <p>Checkboxes</p>
+        <Checkbox checked={true}>Checked</Checkbox>
+        <Checkbox>Unchecked</Checkbox>
+    </section>
 
-    <p>Color</p>
-    <Color />
+    <section>
+        <p>Inputs</p>
+        <Input icon={iconSearch} {iconRight} placeholder="Search..." />
+        <Input disabled />
+        <Input placeholder="Invalid Input" invalid />
+    </section>
 
-    <p>Segment Control</p>
-    <SegmentedControl
-        options={[
-            { id: "twitch", label: "Twitch", icon },
-            { id: "kick", label: "Kick" },
-            { id: "yt", label: "YouTube" },
-            { id: "tt", label: "TikTok" },
-        ]}
-    />
+    <section>
+        <p>Color</p>
+        <Color />
+    </section>
 
-    <p>Selector</p>
-    <Selector options={selectorOptions} />
+    <section>
+        <p>Segment Control</p>
+        <SegmentedControl
+            options={[
+                { id: "twitch", label: "Twitch", icon },
+                { id: "kick", label: "Kick" },
+                { id: "yt", label: "YouTube" },
+                { id: "tt", label: "TikTok" },
+            ]}
+        />
+    </section>
+
+    <section>
+        <p>Selector</p>
+        <Selector options={selectorOptions} />
+    </section>
 </main>
 
 <style>
@@ -71,14 +86,27 @@
         margin: 0;
     }
 
+    h1 {
+        width: 100%;
+    }
+
     main {
         padding: 0.5rem 1rem;
 
+        width: 100%;
+
         display: flex;
-        flex-direction: column;
+        flex-wrap: wrap;
         align-items: flex-start;
         gap: 1rem;
 
         overflow-y: auto;
+
+        section {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 1rem;
+        }
     }
 </style>
