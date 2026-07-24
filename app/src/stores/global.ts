c98be701459589938e1e579c49a7014e3f1c5423
unii <61081555+Fiszh@users.noreply.github.com>
2026-07-24 18:50:08 +0200

    add TangiaBot

--- app/src/stores/global.ts
+++ app/src/stores/global.ts
@@ -101,6 +101,7 @@ export const globals: Globals = {
         "soundalerts", // from speedyemperor
         "rancbot", // from fehleno
         "waga_bot", // from fehleno
+        "tangiabot,
     ],
     /*
     If you want your bot added, open a PR on the repo.
