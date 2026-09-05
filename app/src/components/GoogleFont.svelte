17f8c2f467c8b91052ad1ecfbe4f84bf999c26d0
unii <61081555+Fiszh@users.noreply.github.com>
2026-09-05 12:04:30 +0200

    fix fonts

--- app/src/components/GoogleFont.svelte
+++ app/src/components/GoogleFont.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
     type Props = { fontName: string };
 
     let { fontName = "Geist" }: Props = $props();
 
-    let fontUrl = $state("");
-
-    onMount(() => {
-        const formattedFont = fontName.replace(/\s+/g, "+");
-        fontUrl = `https://fonts.googleapis.com/css2?family=${formattedFont}:wght@400;700&display=swap`;
-
-        console.log(fontName);
-        console.log(formattedFont);
-        console.log(fontUrl);
-    });
+    let fontUrl = $derived(
+        `https://fonts.googleapis.com/css2?family=${fontName.replace(/\s+/g, "+")}:wght@400;700&display=swap`
+    );
 </script>
 
 <svelte:head>
